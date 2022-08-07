8d3f40ee6ede21739f4b4324f9ff148f97b82414
Aleksandr <almoshnik89@gmail.com>
2022-08-07 13:26:35 +0300

    MinMax

---
+++ Examples/Example006_Base/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 100;
+int b = 220;
+int c = 64;
+int d = 8223;
+int e = 443;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write("Max = ");
+Console.WriteLine(max);
