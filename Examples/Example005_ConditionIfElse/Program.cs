6660e0c6a54c94d048336749ee29648e96732fe5
Aleksandr <almoshnik89@gmail.com>
2022-08-07 12:17:35 +0300

    ConditionIfElse

---
+++ Examples/Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите своё имя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "лев")
+{
+    Console.WriteLine("Ку-ку Лев");
+}
+
+else
+{
+    Console.Write("Приветики, ");
+    Console.WriteLine(username);
+}
