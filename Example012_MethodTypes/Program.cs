4cec89e2ebb3667a80d32182ea50d6b6d969ed19
Aleksandr <almoshnik89@gmail.com>
2022-08-22 17:03:48 +0300

    Lesson3

--- Example012_MethodTypes/Program.cs
+++ Example012_MethodTypes/Program.cs
@@ -41,7 +41,7 @@ Console.WriteLine(year); */
 
 //Вид 4. Принимающие и возвращающие методы
 
-string Method4(int count, string text)
+/* string Method4(int count, string text)
 {
     int i = 0;
     string result = String.Empty; //или string result = ""; тоже самое, что string result = String.Empty;
@@ -53,4 +53,121 @@ string Method4(int count, string text)
     return result;
 }
 string res = Method4(10, "q");
-Console.WriteLine(res);
+Console.WriteLine(res); */
+
+
+/* internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Вид 4. Через цикл for
+        string Method4(int count, string text)
+        {
+            string result = string.Empty;
+            for (int i = 0; i < count; i++)
+            {
+                result = result + text;
+            }
+            return result;
+        }
+        string res = Method4(10, "q");
+        Console.WriteLine(res);
+    }
+} */
+
+// Вид 4. Цикл в цикле/ Таблица умножения
+
+
+/* 
+for (int i = 1; i <= 10; i++)
+{
+    for (int j = 1; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i * j}");
+    }
+    Console.WriteLine();
+}
+ */
+
+
+
+
+
+// Работа с текстом
+// Дан текст. В тексте нужно все пробелы заменить черточками,
+// маленькие буквы "к" заменить большими "К",
+// а большие "С" заменить маленькими "с".
+
+/* 
+
+string text = "- Я думаю, - сказал князь, улыбаясь, - что,"
+            + "ежели бы вас послали вместо нашего милого Винценгроде,"
+            + "вы бы взяли приступом согласие прусского короля."
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+//для обращения к строке или её содержимому, можно использовать:
+//string s = "qwerty"
+//            012345
+//для получения конкретного символа, необходимо обратиться к нему так:
+// s [3] // r
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+    int lenght = text.Length;
+    for (int i = 0; i < lenght; i++)
+    {
+        if(text[index: i] == oldValue) result = result + $"{newValue}";
+        else result + $"{text[i]}";
+    }
+
+    return result;
+}
+
+string newText = Replace(text, oldValue: " ", newValue: "|");
+Console.WriteLine(newText);
+
+
+Почему-то не работает.......
+ */ 
+
+
+
+
+
+// Сортировка массива от меньшего к большему
+
+int[] arr = {1, 4, 5, 7, 6, 3, 1, 1};
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    
+    for (int i=0; i < count; i++)
+    {
+        Console.WriteLine($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    for (int i=0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+
+        for(int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] < array[minPosition]) minPosition = j;
+        }
+
+
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
